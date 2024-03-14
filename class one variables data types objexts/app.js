098777d367d6cc9f6c4b6c98bf6ddf2a5c624022
Sufiyan Bawany <sufiyanbaw1256@gmail.com>
2024-03-14 09:25:39 +0500

    files update
    update files

---
+++ class one variables data types objexts/app.js
@@ -0,0 +1,107 @@
+// // console.log('Hello Sufiyan')
+// // Variables
+
+
+// // var a=10;
+// // var b=20;
+
+// // console.log(a+b)
+
+// // Types Of Variables
+
+// // var a=10;
+// // var b=20;
+// // var c="Sufiyan";
+// // var d=null;
+// // var f=undefined;
+
+// // console.log(typeof a , typeof b , typeof c , typeof d, typeof f)
+
+// // Legal or Illegal case
+// // var _a="Sufiyan Mansoor" it is leggal
+// // var 55a=12; its is Ileegal 
+
+// // var Name="Sufiyan Bawany";
+ 
+// // console.log(name)
+
+// // Variables After Declartion its change
+
+// // var a="SufiyanMansoor";
+// // a="Mansoor AHmed"
+
+// // // console.log(a)
+
+// //  a=6;
+// // b=a+1;
+
+// // console.log(b)
+
+// // const a1=6;
+// // a1=a1+1;
+
+// // console.log(a1)
+
+// // Using let 
+
+// let a1=6;
+// a1=a1+2;
+
+// {
+//     let a1=66;
+//     console.log(a1)
+// }
+
+
+// console.log(a1)
+
+// data types in jssss
+
+
+// let x="Sufiyan Mansoor";
+// let y=22;
+// let z=3.45;
+// const p=true;
+// let r=undefined;
+// let v=null;
+
+// console.log(x,y,z,p,r,v)
+// console.log(typeof a,typeof y,typeof z,typeof p,typeof r,typeof v)
+
+
+// Object in java Script
+
+let o={
+    // Key:   Value
+    "name":"Sufiyan Mansoor",
+    "Job code": 5200,
+    "is_Handsome":true,
+    "its empty":null,
+    "its no defined":undefined,
+
+}
+
+console.log(o);
+
+o.salary=20000;
+
+console.log(o)
+
+o.salary=30000;
+
+console.log(o)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
